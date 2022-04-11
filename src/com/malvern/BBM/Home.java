faa772050c14b74113f0090918019089eb124351
sport <sport@192.168.0.220>
2022-04-11 23:09:34 +0300

    Added action to pdf button button so that it can open pdf inside the code. I used Java.io for file handling

--- src/com/malvern/BBM/Home.java
+++ src/com/malvern/BBM/Home.java
@@ -1,7 +1,12 @@
 package com.malvern.BBM;
 
+import java.applet.Applet;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import javax.swing.*;
 
 public class Home implements ActionListener {
@@ -39,6 +44,48 @@ public class Home implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		
+		if(e.getSource() == pdf) {
+			
+			/*try {
+				
+				Runtime.getRuntime().exec("rundll32 url.dll, FileProtocolHandler " + 
+				"ECO_3_lecture_6_Introduction_to_the_Branch_and_Bound_Method.pdf");
+				
+			}catch(Exception error) {
+				
+				JOptionPane.showMessageDialog(null, "There is a problem with opening file!!", "Error", JOptionPane.ERROR_MESSAGE);
+				
+			}*/
+			BufferedInputStream in = null;
+	        FileOutputStream fout = null;
+	        try { 
+	            in = new BufferedInputStream(getClass().getResource("ECO_3_lecture_6_Introduction_to_the_Branch_and_Bound_Method.pdf").openStream());
+	            fout = new FileOutputStream("ECO_3_lecture_6_Introduction_to_the_Branch_and_Bound_Method.pdf");
+	            byte data[] = new byte[1024];
+	            int count;
+	            while ((count = in.read(data, 0, 1024)) != -1) {
+	                fout.write(data, 0, count);
+	            }
+	        } catch (IOException ex) {
+	            Logger.getLogger(Applet.class.getName()).log(Level.SEVERE, null, ex);
+	        } finally {
+	            try {
+	                if ( in != null)
+	                    in.close();
+	                if (fout != null) {
+	                    fout.close();
+	                    if (Desktop.isDesktopSupported()) {
+	                        File file = new File(System.getProperty("user.dir")+"\\ECO_3_lecture_6_Introduction_to_the_Branch_and_Bound_Method.pdf");
+	                        Desktop.getDesktop().open(file);
+	                    }
+	                }
+	            }
+	            catch (IOException ex) {
+	                Logger.getLogger(Applet.class.getName()).log(Level.SEVERE, null, ex);
+	            }
+	        }
+		}
 		
 	}
 
